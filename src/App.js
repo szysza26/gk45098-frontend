31de0b01f197504487330b42e51deebb681d8dd9
Krzysiek <szy162@gmail.com>
2022-02-03 11:39:50 +0100

    add material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { makeStyles } from '@mui/styles';
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+    color: 'red',
+  },
+});
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      Hello World
     </div>
   );
 }
